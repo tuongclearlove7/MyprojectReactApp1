30fd29b7285b2cbf88538ac3290cd852dbe6e27d
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-12-21 05:12:45 +0700

    update source

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -10,13 +10,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-              <UserProvider>
-                  <App />
-              </UserProvider>
-      </BrowserRouter>
-  </React.StrictMode>
+      <React.StrictMode>
+          <BrowserRouter>
+                  <UserProvider>
+                      <App />
+                  </UserProvider>
+          </BrowserRouter>
+      </React.StrictMode>
 );
 
 reportWebVitals();
